f113e7df24127180dcb7c5d35ecd0ef723d1f437
Syeilla <151445182+Syeilla@users.noreply.github.com>
2023-11-20 13:36:40 +0700

    Add files via upload

---
+++ uts/UTS.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int BiayaParkir (string Jeniskendaraan, int DurasiParkir){
+    int Biayaperjam = (Jeniskendaraan == "motor") ? 2000 : 5000;
+    return Biayaperjam * DurasiParkir;
+
+}
+
+
+int main (){
+
+    int Parkirmobil = 5000;
+    int Parkirmotor = 2000;
+    int jumlahMotor = 2;
+    int jumlahMobil = 3;
+    int Jammasuk = 15;
+    int Jamkeluar = 14;
+
+       cout << "masukkan jumlah motor : ";
+    cin >> jumlahMotor;
+    cout << "masukkan jumlah mobil : ";
+    cin >> jumlahMobil;
+    cout << "input jam masuk (format 24 jam) : ";
+    cin >> Jammasuk;
+    cout << "input jam keluar (format 24 jam) : ";
+    cin >> Jamkeluar;
+    cout << "\n";
+
+    int DurasiParkir = (Jamkeluar + 24 - Jammasuk) % 24;
+    int totalPendapatanMotor = BiayaParkir("motor", DurasiParkir) * jumlahMotor;
+    int totalPendapatanMobil = BiayaParkir("mobil", DurasiParkir) * jumlahMobil;
+    int totalPendapatan = totalPendapatanMotor + totalPendapatanMobil;
+
+    cout << "Total Biaya Parkir: Rp. " << totalPendapatan << "(" << DurasiParkir << " jam)";
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+}
